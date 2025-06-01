501824dc466360bc3de97554d773a7e49615c26c
divine-mercyx <divinewilson766@gmail.com>
2025-06-01 02:46:00 +0100

    update on urlShortener - urlController, added gitignore file

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDb from "./config/db.js";
+import userRoute from "./routes/userRoute.js";
+import urlRoute from "./routes/urlRoute.js";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = process.env.PORT || 3000;
+app.use("/api/v1", userRoute)
+app.use("/api/v1/url", urlRoute)
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
+connectDb()
